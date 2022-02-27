7934078ede71a152b2317022e15e573125958fd3
Daniel Españadero <developer@daniel-espanadero.com>
2022-02-27 01:51:43 +0100

    Installation of babel for ES6

--- src/models/server.js
+++ src/models/server.js
@@ -19,7 +19,7 @@ class Server {
     };
 
     listen() {
-        this.app.listen(this.port, () => { 
+        this.app.listen(this.port, () => {
             console.log(`Listening on port ${this.port}`);
         });
     };
